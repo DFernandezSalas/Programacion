0276d8bf230b1a922ae763f22848e34c3e9c3526
DFernandezSalas <0301dfernandez@e-itaca.es>
2024-02-26 18:15:22 +0100

    26/02/2024v1

--- Ejercicios2/src/Ejercicio15.java
+++ Ejercicios2/src/Ejercicio15.java
@@ -1,5 +1,3 @@
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
 import java.util.Scanner;
 
 public class Ejercicio15 {
@@ -8,7 +6,9 @@ public class Ejercicio15 {
 		boolean salir = false;
 		boolean juego = false;
 		int jPuntuacion = 0;
+		int jPuntuacionSet = 0;
 		int cpuPuntuacion = 0;
+		int cpuPuntuacionSet = 0;
 		Scanner reader = new Scanner(System.in);
 		int respuesta = 0;
 		int seleccion = 0;
@@ -24,27 +24,67 @@ public class Ejercicio15 {
 				limpiar();
 				while (juego == false) {
 					do {
-						System.out.println("Marcador\nJugador = " + jPuntuacion + "\t CPU = " + cpuPuntuacion);
+						System.out.println("MARCADOR (PUNTOS/SETS)\nJugador = " + jPuntuacion + "/" + jPuntuacionSet
+								+ "\t CPU = " + cpuPuntuacion + "/" + cpuPuntuacionSet);
 						System.out.print("1.Piedra \n2.Papel \n3.Tijera \nRespuesta: ");
 						seleccion = reader.nextInt();
 					} while (seleccion < 1 || seleccion > 3);
 					switch (seleccion) {
 					case 1: {
+						limpiar();
 						jugador = 0;
 						cpu = cpu();
 						if (jugador == cpu) {
-							
+							System.out.println("=============EMPATE=============");
+							System.out.println("Jugador-> Piedra vs Piedra <-CPU");
+						} else if (cpu == 1) {
+							System.out.println("===========CPU GANA=============");
+							System.out.println("Jugador-> Piedra vs Papel <-CPU");
+							cpuPuntuacion++;
+						} else if (cpu == 2) {
+							System.out.println("===========JUGADOR GANA=============");
+							System.out.println("Jugador-> Piedra vs Tijera <-CPU");
+							jPuntuacion++;
 						}
+						comprobar(jPuntuacion, cpuPuntuacion, jPuntuacionSet, cpuPuntuacionSet);
 						break;
 					}
 					case 2: {
+						limpiar();
 						jugador = 1;
 						cpu = cpu();
+						if (jugador == cpu) {
+							System.out.println("=============EMPATE=============");
+							System.out.println("Jugador-> Papel vs Papel <-CPU");
+						} else if (cpu == 0) {
+							System.out.println("=========JUGADOR GANA===========");
+							System.out.println("Jugador-> Papel vs Piedra <-CPU");
+							jPuntuacion++;
+						} else if (cpu == 2) {
+							System.out.println("===========CPU GANA=============");
+							System.out.println("Jugador-> Papel vs Tijera <-CPU");
+							cpuPuntuacion++;
+						}
+						comprobar(jPuntuacion, cpuPuntuacion, jPuntuacionSet, cpuPuntuacionSet);
 						break;
 					}
 					case 3:
+						limpiar();
 						jugador = 2;
 						cpu = cpu();
+						if (jugador == cpu) {
+							System.out.println("=============EMPATE=============");
+							System.out.println("Jugador-> Tijera vs Tijera <-CPU");
+						} else if (cpu == 0) {
+							System.out.println("===========CPU GANA=============");
+							System.out.println("Jugador-> Tijera vs Piedra <-CPU");
+							cpuPuntuacion++;
+						} else if (cpu == 1) {
+							System.out.println("=========JUGADOR GANA===========");
+							System.out.println("Jugador-> Tijera vs Papel <-CPU");
+							jPuntuacion++;
+						}
+						comprobar(jPuntuacion, cpuPuntuacion, jPuntuacionSet, cpuPuntuacionSet);
 						break;
 					}
 				}
@@ -73,4 +113,29 @@ public class Ejercicio15 {
 		return jugada;
 	}
 
+	public static int comprobar(int jPuntuacion, int cpuPuntuacion, int jPuntuacionSet, int cpuPuntuacionSet) {
+		if (jPuntuacion == 5) {
+			System.out.println("SET para el JUGADOR");
+			jPuntuacion = 0;
+			cpuPuntuacion = 0;
+			jPuntuacionSet++;
+			if (jPuntuacionSet == 3) {
+				System.out.println("JUGADOR GANA el PARTIDO");
+				cpuPuntuacionSet = 0;
+				jPuntuacionSet = 0;
+			}
+		}
+		if (cpuPuntuacion == 5) {
+			System.out.println("SET para la CPU");
+			jPuntuacion = 0;
+			cpuPuntuacion = 0;
+			cpuPuntuacionSet++;
+			if (cpuPuntuacionSet == 3) {
+				System.out.println("CPU GANA el PARTIDO");
+				cpuPuntuacionSet = 0;
+				jPuntuacionSet = 0;
+			}
+		}
+		return new comprobar(jPuntuacion, cpuPuntuacion, jPuntuacionSet, cpuPuntuacionSet);
+	}
 }
