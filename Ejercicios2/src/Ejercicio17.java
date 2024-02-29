ffd83268e3d50ba6dd9ef3c92e0bc0c6877ae1d2
DFernandezSalas <0301dfernandez@e-itaca.es>
2024-02-29 19:25:55 +0100

    29/02/2024v1

--- Ejercicios2/src/Ejercicio17.java
+++ Ejercicios2/src/Ejercicio17.java
@@ -1,11 +1,28 @@
 import javax.swing.JOptionPane;
 
 public class Ejercicio17 {
-	//Simulador de Cajero Automático
+	// Simulador de Cajero Automático
 	public static void main(String[] args) {
-		
-		JOptionPane.showInputDialog("Hola");
+		double dinero = 1000;
+
+		String opcion = JOptionPane.showInputDialog("¿Que operacion deseas realizar? \n Tienes" + dinero
+				+ "€ \n 1.Retirar dinero \n 2.Ingresar dinero \n 3.Salir");
+		int res = Integer.parseInt(opcion);
+		switch (res) {
+		case 1: {
+
+			break;
+		}
+		case 2: {
+
+			break;
+		}
+		case 3: {
+
+			break;
+		}
+
+		}
 
 	}
-
 }
